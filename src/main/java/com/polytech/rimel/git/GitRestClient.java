c09fbba8089a4b5bd92237c4c56bc6ee84ab8966
danial <azswad20@gmail.com>
2018-01-16 07:51:54 +0100

    Minor error

--- src/main/java/com/polytech/rimel/git/GitRestClient.java
+++ src/main/java/com/polytech/rimel/git/GitRestClient.java
@@ -12,7 +12,7 @@ public class GitRestClient {
     private static final String BASE_URL = "https://api.github.com/repos/";
     private static final String CLIENT_ID= "";
     private static final String CLIENT_SECRET = "";
-    private static final String GIT_CRED = "client_id="+CLIENT_ID+"&client_secret="+CLIENT_SECRET+;
+    private static final String GIT_CRED = "client_id="+CLIENT_ID+"&client_secret="+CLIENT_SECRET;
     private static final String PER_PAGE = "per_page=100";
     private static final String SHA = "sha=";
 
