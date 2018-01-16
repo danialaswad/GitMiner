6c019cefe04a4fa59419434ec82f5d25830a3efb
danial <azswad20@gmail.com>
2018-01-16 11:38:14 +0100

    Windows path problem

--- src/main/java/com/polytech/rimel/mine/Miner.java
+++ src/main/java/com/polytech/rimel/mine/Miner.java
@@ -69,6 +69,9 @@ public class Miner {
         if (outputPath.toString().equals("")) {
             throw new GitRestClientException("Please provide an output path");
         }
+        if (!isAccessibility(outputPath.toString())) {
+            throw new GitRestClientException("Please provide a valid output path");
+        }
 
         try {
             LOGGER.log(Level.INFO, "Retrieving commits history for " + filePath + " from " + owner + "/" + repository);
